f7fd91cc0bf60d0f92814a9272735b3a6a43ef90
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2026-08-22 23:01:40 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+
+    printf("Exercicio 2: calculo de imc\n");
+    float peso, altura;
+    printf("Digite seu peso em kg: \n");
+    scanf("%f", &peso);
+    printf("digite sua altura em metros: \n");
+    scanf("%f", &altura);
+
+    float imc = peso / (altura * altura);
+    printf("O IMC de uma pessoa com peso %f kg e altura %f m é igual a %f. \n", peso, altura, imc);
+  
+  return 0;
+}
