9b05ff244ba38f1adb4279b0c67854721342b9d0
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2026-08-22 23:00:15 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+     printf("Exercicios em aula \n");
+    
+    printf("Exercicio 1: operações aritmeticas\n");
+    int num1, num2;
+    printf("Insira o primeiro numero inteiro: \n");
+    scanf("%d", &num1);
+    printf("Insira o segundo numero inteiro: \n");
+    scanf("%d", &num2);
+
+    int soma, sub, mult;
+    float div;
+    
+    soma = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+
+    printf("%d é o valor da soma \n", soma);
+    printf("%d é o valor da subtração \n", sub);
+    printf("%d é o valor da multiplicação \n", mult);
+    printf("%f é o valor da divisão \n", div);
+
+    return 0;
+}
