ce626907e5e0453efa4198046afa76b4fb3c3369
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2026-08-22 23:03:22 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+void exibesituacaoacademica (float media, int porcentagem) {
+    if (porcentagem < 75) {
+        printf("REPROVADO \n");
+    }
+    else if (porcentagem > 75 && media <= 7.5) {
+        printf("DE EXAME\n");
+    }
+    else {
+        printf("APROVADO \n");
+    }
+}
+
+int main() {
+
+  printf("Exercicio 3: média \n");
+    
+    float media;
+    int porcentagem;
+
+    printf("digite sua média: \n");
+    scanf("%f", &media);
+    printf("Digite sua frequência: \n");
+    scanf("%d", &porcentagem);
+
+    exibesituacaoacademica(media, porcentagem);
+  
+  return 0;
+}
